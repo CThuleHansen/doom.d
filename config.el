376c9a7cb75a7923f95ee4c262f7dacdc700aeb0
Casper Thule <casper.thule@eng.au.dk>
2018-11-29 12:51:35 +0100

    Added manual haskell configuration

--- config.el
+++ config.el
@@ -7,11 +7,20 @@
 ;; http://haskell.github.io/haskell-mode/manual/latest/Interactive-Haskell.html
 (require 'haskell-interactive-mode)
 (require 'haskell-process)
-(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+(require 'flycheck)
+
+;; The default checker is haskell-stack-ghc but haskell-ghc should be used instead.
+;; http://www.flycheck.org/en/latest/user/syntax-checkers.html
+(add-hook 'haskell-mode-hook 'hm-hooks)
+(defun hm-hooks ()
+  "My Haskell-mode-hooks"
+  (interactive-haskell-mode)
+  (flycheck-select-checker 'haskell-ghc)
+  )
 ;; Using cabal's integrated REPL capabilities instead of ghci
 ;; https://github.com/serras/emacs-haskell-tutorial/blob/master/tutorial.md
 (custom-set-variables
-  '(haskell-process-type "cabal-new-repl")
-  '(haskell-process-log t)
-  '(haskell-compile-cabal-build-command  "cabal new-build --ghc-option=-ferror-spans")
-  )
+ '(haskell-process-type "cabal-new-repl")
+ '(haskell-process-log t)
+ '(haskell-compile-cabal-build-command  "cabal new-build --ghc-option=-ferror-spans")
+ )
