743fcd455def00ef657e49e72161696039e83af6
Casper Thule <casper.thule@eng.au.dk>
2018-11-30 13:41:26 +0100

    Fixed issue with haskell mode and dante

--- config.el
+++ config.el
@@ -4,23 +4,46 @@
 ;; Enables projectile-switch-project to allow opening a folder
 (setq projectile-switch-project-action 'projectile-dired)
 
-;; http://haskell.github.io/haskell-mode/manual/latest/Interactive-Haskell.html
-(require 'haskell-interactive-mode)
-(require 'haskell-process)
-(require 'flycheck)
+;; MANUAL HASKELL-MODE SETUP
 
-;; The default checker is haskell-stack-ghc but haskell-ghc should be used instead.
-;; http://www.flycheck.org/en/latest/user/syntax-checkers.html
-(add-hook 'haskell-mode-hook 'hm-hooks)
-(defun hm-hooks ()
-  "My Haskell-mode-hooks"
-  (interactive-haskell-mode)
-  (flycheck-select-checker 'haskell-ghc)
-  )
-;; Using cabal's integrated REPL capabilities instead of ghci
-;; https://github.com/serras/emacs-haskell-tutorial/blob/master/tutorial.md
-(custom-set-variables
- '(haskell-process-type "cabal-new-repl")
- '(haskell-process-log t)
- '(haskell-compile-cabal-build-command  "cabal new-build --ghc-option=-ferror-spans")
- )
+;; http://haskell.github.io/haskell-mode/manual/latest/Interactive-Haskell.html
+;;(require 'haskell-interactive-mode)
+;;(require 'haskell-process)
+;;(require 'flycheck)
+;;
+;;;; The default checker is haskell-stack-ghc but haskell-ghc should be used instead.
+;;;; http://www.flycheck.org/en/latest/user/syntax-checkers.html
+;;(add-hook 'haskell-mode-hook 'hm-hooks)
+;;(defun hm-hooks ()
+;;  "My Haskell-mode-hooks"
+;;  (interactive-haskell-mode)
+;;  (flycheck-select-checker 'haskell-ghc)
+;;  )
+;;;; Using cabal's integrated REPL capabilities instead of ghci
+;;;; https://github.com/serras/emacs-haskell-tutorial/blob/master/tutorial.md
+;;(custom-set-variables
+;; '(haskell-process-type "cabal-new-repl")
+;; '(haskell-process-log t)
+;; '(haskell-compile-cabal-build-command  "cabal new-build --ghc-option=-ferror-spans")
+;; )
+
+;; DANTE SETUP
+;;(add-hook 'haskell-mode-hook 'dante-configure)
+;;
+;;
+;;(defun dante-configure ()
+;;  'dante-mode
+;;  'flycheck-mode)
+;;
+;;(setq flymake-no-changes-timeout nil)
+;;(setq flymake-start-syntax-check-on-newline nil)
+;;(setq flycheck-check-syntax-automatically '(save mode-enabled))
+;;
+;; MANUAL DANTE HASKELL MODE SETUP
+;;
+(require 'haskell-mode)
+(add-hook 'haskell-mode-hook 'dante-mode)
+(add-hook 'haskell-mode-hook 'flycheck-mode)
+(setq flymake-no-changes-timeout nil)
+(setq flymake-start-syntax-check-on-newline nil)
+(setq flycheck-check-syntax-automatically '(save mode-enabled))
