d00c7cd10012cb12f898bb395ef438f29a6f6988
Casper Thule <casper.thule@eng.au.dk>
2019-09-26 22:53:32 +0200

    Added readme and config reftex and tex keywords

--- config.el
+++ config.el
@@ -6,39 +6,33 @@
 (after! ispell (setq ispell-dictionary "en_GB"))    ;set the default dictionary
 
 ;; LATEX CONFIGURATION
-;(def-package! tex
-;  :config
-;  (setq TeX-PDF-mode t)
-;  ;; Generate sync file and sync with C-v
-;  (eval-after-load
-;      "tex" '(add-to-list 'TeX-command-list
-;                          '("latexmk" "latexmk -pdf %t --synctex=1" TeX-run-TeX)))
-;
-;  ;; Enable spell-checking
-;  (add-hook 'LaTeX-mode-hook 'flyspell-mode)
-;  (add-hook 'LaTeX-mode-hook
-;            '(lambda ()
-;               (reftex-mode)))
+(def-package! tex
+  :config
+  ;; Enable spell-checking
+  (add-hook 'LaTeX-mode-hook 'flyspell-mode)
+                                        ;  (add-hook 'LaTeX-mode-hook
+                                        ;            '(lambda ()
+                                        ;               (reftex-mode)))
 ;;; LateX keywords that need colouring
-;(setq font-latex-match-reference-keywords
-;      '(
-;        ("ac" "[{")
-;        ("todo" "[{")
-;        ("kw" "[{")
-;        ("vsl" "[{")
-;        ("vpp" "[{")
-;        ("vrt" "[{")
-;        ("acp" "[{")
-;        ("contribution" "[{")
-;        ("acrodef" "[{")
-;        ("crefName" "[{")
-;        ("cref" "[{")
-;        ("Cref" "[{")
-;        ("cnref" "[{")
-;        ("vdmkw" "[{")
-;        ))
-;)
-;
+  (setq font-latex-match-reference-keywords
+        '(
+          ("ac" "[{")
+          ("todo" "[{")
+          ("kw" "[{")
+          ("vsl" "[{")
+          ("vpp" "[{")
+          ("vrt" "[{")
+          ("acp" "[{")
+          ("contribution" "[{")
+          ("acrodef" "[{")
+          ("crefName" "[{")
+          ("cref" "[{")
+          ("Cref" "[{")
+          ("cnref" "[{")
+          ("vdmkw" "[{")
+          ))
+  )
+
 
 ;; Necessary such that projectile does not attempt to contact SVN server
 ;;https://github.com/bbatsov/projectile/issues/520
--- readme.md
+++ readme.md
@@ -2,9 +2,14 @@
 
 ## Connection between flyspell, ispell, aspell and similar
 
-Ispell is the original application for spellchecking
-    It is both a backend application and an integrated part of emacs.
-Aspell is a better version of the backend application ispell and offers a
-    similar interface.
-Flyspell is an emacs package with additional spell-checking functionality and
+ispell.el allows on to use external spell checker programs inside
+Emacs. This is an integrated part of Emacs.
+
+ispell, aspell, hunspell are external spell checking programs.
+
+flyspell.el performs on-the-fly spell checking.
+ispell.el is completely manual.
+
+Both ispell.el and flyspell.el requires an external
+
 
